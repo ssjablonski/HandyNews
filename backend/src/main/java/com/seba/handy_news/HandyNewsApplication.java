2d1b815262fd7dc1cbf72ad7119d425267969a84
Sebastian Jablonski <sebaa1080@gmail.com>
2024-11-28 23:35:00 +0100

    Initial backend commit

---
+++ backend/src/main/java/com/seba/handy_news/HandyNewsApplication.java
@@ -0,0 +1,13 @@
+package com.seba.handy_news;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HandyNewsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HandyNewsApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/seba/handy_news/HandyNewsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seba.handy_news;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HandyNewsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
